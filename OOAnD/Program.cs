d584c3aa28f63b6a2167757c3eee1a109a247adf
Maksim <maximben888@gmail.com>
2026-02-27 16:43:58 +0600

    add files

---
+++ OOAnD/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOAnD;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using OOAnD;
+using FluentAssertions;
+
+namespace tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+    {
+        int a = 5;
+        int b = 7;
+        
+        int result = Calculator.Add(a, b);
+        
+        result.Should().Be(12);
+    }
+}
